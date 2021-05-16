3c3c86a5e6e2e3f8aebb4dc3c3ba92fbbc8424ce
s-sarkissian <sevan.sarkissian@student.isae-supaero.fr>
2021-05-16 20:01:59 +0200

    add template for album selection still database problems

--- disks/models.py
+++ disks/models.py
@@ -2,17 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-class Track(models.Model):
-    id = models.IntegerField(),
+class Artist(models.Model):
     name = models.CharField(max_length=200),
-    composer = models.CharField(max_length=200),
-    milliseconds = models.IntegerField(),
-    bytes = models.IntegerField(),
-    unitPrice = models.DecimalField(),
-    album = models.ForeignKey('Album', on_delete=models.SET_NULL),
+    id = models.BigIntegerField(),
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'artist'
 
     def __str__(self):
         return self.name
@@ -21,21 +17,28 @@ class Track(models.Model):
 class Album(models.Model):
     title = models.CharField(max_length=200),
     id = models.IntegerField(),
-    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'album'
 
     def __str__(self):
         return self.title
 
 
-class Artist(models.Model):
+class Track(models.Model):
+    id = models.IntegerField(),
     name = models.CharField(max_length=200),
-    id = models.BigIntegerField(),
+    composer = models.CharField(max_length=200),
+    milliseconds = models.IntegerField(),
+    bytes = models.IntegerField(),
+    unitPrice = models.DecimalField(),
+    album = models.ForeignKey(Album, on_delete=models.SET_NULL),
 
     class Meta:
         ordering = ['id']
+        verbose_name = 'track'
 
     def __str__(self):
         return self.name
--- disks/views.py
+++ disks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
-from .models import Album
-
+from disks.models import Album
+from disks.models import Track
 
 # Create your views here.
 
@@ -11,10 +11,11 @@ def album(request):
     return render(request, 'disks/album.html', {'albums_disponibles': albums})
 
 
-def select(request, id):
+def select(request, selected_id):
     try:
-        albums = Album.objects.get(id=id)
+        albums = Album.objects.get(id=selected_id)
+        tracks = Track.objects.get(album=selected_id)
     except Album.DoesNotExist:
         raise Http404
 
-    return render(request, 'disks/album.html', {'album': albums}),
+    return render(request, 'disks/track.html', {'album': albums}, {'track': tracks}),
